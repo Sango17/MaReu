8fd08d2751d6768db200c8048200baefe4fb233e
Alex <alexandre.seneviratne@gmail.com>
2020-02-21 13:13:55 +0100

    Small fix

--- app/src/androidTest/java/com/alexandreseneviratne/mareu/listMeetingInstrumentedTest.java
+++ app/src/androidTest/java/com/alexandreseneviratne/mareu/listMeetingInstrumentedTest.java
@@ -3,6 +3,7 @@ package com.alexandreseneviratne.mareu;
 import android.widget.DatePicker;
 
 import androidx.test.espresso.contrib.PickerActions;
+import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
 
@@ -10,6 +11,7 @@ import com.alexandreseneviratne.mareu.model.Date;
 import com.alexandreseneviratne.mareu.model.Meeting;
 import com.alexandreseneviratne.mareu.model.Time;
 import com.alexandreseneviratne.mareu.ui.MainActivity;
+import com.alexandreseneviratne.mareu.utils.DeleteItemViewAction;
 
 import org.hamcrest.Matchers;
 import org.junit.Before;
@@ -23,6 +25,7 @@ import java.util.ArrayList;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.RootMatchers.isPlatformPopup;
 import static androidx.test.espresso.matcher.ViewMatchers.assertThat;
 
 import static androidx.test.espresso.matcher.ViewMatchers.hasMinimumChildCount;
@@ -101,6 +104,8 @@ public class listMeetingInstrumentedTest {
         mActivity.meetingApiService.addMeeting(testMeetingReservation2);
         mActivity.meetingApiService.addMeeting(testMeetingReservation3);
         mActivity.meetingApiService.addMeeting(testMeetingReservation4);
+
+        mActivity.toList();
     }
 
     @Test
@@ -123,11 +128,17 @@ public class listMeetingInstrumentedTest {
         // Select date (14/02/2020)
         onView(withClassName(Matchers.equalTo(DatePicker.class.getName())))
                 .perform(PickerActions.setDate(2020, 2, 14));
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         onView(withText("OK")).perform(click());
 
         // Select period of time (9h00 - 10h00)
         onView(withId(R.id.dialog_filter_spinner)).perform(click());
-        onView(withText("9h00 – 10h00")).perform(click());
+        //onView(withText("9h00 – 10h00")).perform(click());
+        onView(withText("9h00 – 10h00")).inRoot(isPlatformPopup()).perform(click());
 
         // Click to validate
         onView(withId(R.id.dialog_filter_accept)).perform(click());
@@ -150,12 +161,26 @@ public class listMeetingInstrumentedTest {
 
         // Select hall - Peach
         onView(withId(R.id.dialog_filter_spinner)).perform(click());
-        onView(withText("Peach")).perform(click());
+        //onView(withText("Peach")).perform(click());
+        onView(withText("Peach")).inRoot(isPlatformPopup()).perform(click());
 
         // Click to validate
         onView(withId(R.id.dialog_filter_accept)).perform(click());
 
         // Check count of filtered list - only two are remaining at Peach hall
         onView(withId(R.id.list_recycler_view)).check(matches(hasMinimumChildCount(2)));
+    }
+
+    @Test
+    public void listFragment_deleteAction_deleteMeetingFromList() {
+        // Check if there are 4 meeting reservations in the list
+        onView(withId(R.id.list_recycler_view)).check(matches(hasMinimumChildCount(4)));
+
+        // Delete the first meeting of the list
+        onView(withId(R.id.list_recycler_view))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(0, new DeleteItemViewAction()));
+
+        // Check if there are 3 meeting reservations remaining in the list
+        onView(withId(R.id.list_recycler_view)).check(matches(hasMinimumChildCount(3)));
     }
 }
